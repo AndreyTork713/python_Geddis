15c30f8e8d5ce6b1819689fd536b6d669a87c3fb
Andrey Kropotin <tork713@gmail.com>
2023-02-24 12:25:35 +0700

    Пока не работает...

---
+++ return_list.py
@@ -0,0 +1,30 @@
+def main():
+
+
+    # Получить список с хранящимися в нем значениями
+    numbers = get_values()
+
+    # Показать значения в списке.
+    print('Значения в списке: ')
+    print(numbers)
+
+
+    # Функция get_values() получает от пользователя ряд значений
+    # и сохраняет их в списке.
+    # эта функция возвращает ССЫЛКУ на список.
+
+def get_values():
+    values = []
+    again = 'y'
+    while again == 'y':
+        for value in values:
+            value = input('Введите значение от 1 до 10: ')
+            values.append(value)
+            again = input('Continue y/n: ')
+            print()
+    return values
+
+
+if __name__ == '__main__':
+    main()
+
