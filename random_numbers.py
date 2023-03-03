7eb383e71e430e7eb3cf6ca79415a781907a0a35
Andrey Kropotin <tork713@gmail.com>
2023-03-03 07:54:00 +0700

    двумерные списки

---
+++ random_numbers.py
@@ -0,0 +1,19 @@
+import random
+ROWS = 3    # Определить количество строк
+COLS = 4   # Определить количество колонок
+def main():
+    # Создать "нулевой список"
+    values = [[0, 0, 0, 0],
+              [0, 0, 0, 0],
+              [0, 0, 0, 0]]
+
+    # Заполнить список случайными значениями
+    for r in range(ROWS):
+        for c in range(COLS):
+            values[r][c] = random.randint(1, 100)
+
+    print(values)
+
+
+if __name__ == '__main__':
+    main()
---
+++ two_dimensional_list.py
@@ -0,0 +1,15 @@
+def main():
+
+    # Создать двумерный список
+    values = [[1, 2, 3],
+              [10, 20, 30],
+              [100, 200, 300]]
+
+    # Вывести на экран список
+    for row in values:
+        for element in row:
+            print(element)
+
+                       
+if __name__ == '__main__':
+    main()
